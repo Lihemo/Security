0a4a51c96a1fcdad67587d0356225a64b229e773
John Lihemo <johneshiwani29@gmail.com>
2018-08-18 19:38:35 +0300

    The first committ

---
+++ main.js
@@ -0,0 +1,9 @@
+//single line comments are delimited using the '//' signs in js. 
+$.substitute () {
+    var myValue = document.getElementById("mytextbox").value;
+
+    if (myValue.length === 0) {
+        alert("Please enter a real value then submit!!");
+        return;
+    }
+}
