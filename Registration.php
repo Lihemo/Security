b703b2f680a7d84a6c19969158476156005dde3a
John Lihemo <johneshiwani29@gmail.com>
2019-02-15 18:59:43 +0300

    Added bootstrap and font awesome

--- Registration.php
+++ Registration.php
@@ -1,130 +1,153 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Registration Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="registration.css" />
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="registration.js"></script>
+    <script src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </head>
+
 <body>
-   <h1>Registration Forms</h1>
+     <!--this is the navigation bar -->
+     <div class="container">
+            <nav class="navbar navbar-dark navbar-expand-md bg-dark">
+                <button type="button" class="nav-toggler" data-toggle="collapse" data-target="#myNav"><span class="navbar-toggler-icon"></span></button>
+                <div id="myNav" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a href="index.php" class="nav-link">HomePage</a></li>
+                        <li class="nav-item"><a href="Registration.php" class="nav-link">RegistrationPage</a></li>
+                        <li class="nav-item"><a href="https://www.google.com" class="nav-link">Google</a></li>
+                        <li class="nav-item"><a href="https://www.youtube.com" class="nav-link">YouTube</a></li>
+                        <li class="nav-item"><a href="https://www.youtube.com" class="nav-link">Contacts</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div><br />
+        <div class="h1 text-center text-primary"><u>Registration Forms</u></div>
 
-   <nav>
-        <ul>
-            <li> <a href="index.php" title="This is the HomePage"> Homepage</a> </li>
-            <li> <a href="Registration.php" title="This is the Sign in Page"> RegisterPage</a> </li>
-            <li> <a href="http://www.Google.com" title="Google.com"> Google.com</a> </li>
-            <li> <a href="http://www.YouTube.com" title="YouTube.com"> YouTube</a> </li>  
-        </ul>
-    </nav>
+    <div class="container jumbotron">
+        <div class="row">
+            <div class="col">
+                <!--this code is for the students table-->
+                <Form method="POST" action="php_scripts/student.php" id="student" onsubmit="return student ()">
+                    <div class="h3 text-center"><u>Student Form</u></div>
+                    <label>Name:</label>
+                    <input type="text" id="jina" name="jina"> <br /> <br />
+                    <label for="">Course:</label>
 
-<!--this code is for the students table-->
-    <Form method="POST" action="php_scripts/student.php" id="student" onsubmit="return student ()">
-                <h2>Student Registration Form</h2>
-            <label>Name:</label>
-            <input type="text" id = "jina" name="jina" > <br /> <br />
-            <label for="">Course:</label>
+                    <!--this code below is for the dropdown menu-->
+                    <select name="john" id="john">
+                        <option value="Diploma in ICT">Diploma in ICT</option>
+                        <option value="Bachelors in ICT">Bachelors in ICT</option>
+                        <option value="Bachelors in Mechanical Engineering">Bachelors in Mechanical Engineering</option>
+                        <option value="Diploma in Animal Husbandry">Diploma in Animal Husbandry</option>
+                    </select> <br /> <br />
+                    <!--it ends here-->
 
-             <!--this code below is for the dropdown menu-->
-             <select name="john" id="john">
-                <option value="Diploma in ICT">Diploma in ICT</option>
-                <option value="Bachelors in ICT">Bachelors in ICT</option>
-                <option value="Bachelors in Mechanical Engineering">Bachelors in Mechanical Engineering</option>
-                <option value="Diploma in Animal Husbandry">Diploma in Animal Husbandry</option> 
-            </select> <br /> <br />
-            <!--it ends here-->  
+                    <label for="">Laptop Model:</label>
+                    <input type="text" id="model" name="model" placeholder="Manufacturer's Name"> <br /> <br />
+                    <label>Serial Number:</label>
+                    <input type="text" id="serial" name="serial"> <br /> <br />
+                    <button class="btn btn-primary">Submit</button>
+                </Form>
+            </div>
 
-            <label for="">Laptop Model:</label>
-            <input type="text" id = "model" name="model" placeholder="Manufacturer's Name" > <br /> <br />
-            <label>Serial Number:</label>
-            <input type="text" id = "serial" name="serial"> <br /> <br />
+            <div class="col">
+                <!--this code is for the lecturer table-->
+                <Form method="POST" action="php_scripts/lecturer.php" id="lecturer" onsubmit="return lecturer ()">
+                    <div class="h3 text-center"><u>Lecturer Form</u></div>
+                    <label>Name:</label>
+                    <input type="text" id="car" name="car"> <br /> <br />
+                    <label for="">Faculty:</label>
 
-            <input type="submit" value="submit" name="ClickMe_one" id = "ClickMe" name="ClickMe_one">
-    
-    </Form>
+                    <!--this code below is for the dropdown menu-->
+                    <select name="chair" id="chair">
+                        <option value="Faculty of IT and Computing">Faculty of IT and Computing</option>
+                        <option value="Faculty of Applied Sciences">Faculty of Applied Sciences</option>
+                        <option value="Faculty of Agriculture">Faculty of Agriculture</option>
+                        <option value="Faculty of Engineering">Faculty of Engineering</option>
+                    </select> <br /> <br />
+                    <!--it ends here-->
 
+                    <label for="">Laptop Model:</label>
+                    <input type="text" id="old" name="old" placeholder="Manufacturer's Name"> <br /> <br />
+                    <label>Serial Number:</label>
+                    <input type="text" id="gender" name="gender"> <br /> <br />
+                    <button class="btn btn-primary">Submit</button>
 
-<!--this code is for the lecturer table-->
-    <Form method="POST" action="php_scripts/lecturer.php" id ="lecturer" onsubmit="return lecturer ()">
-                <h2>Lecturer Registration Form</h2>
-            <label>Name:</label>
-            <input type="text" id = "car" name="car" > <br /> <br />
-            <label for="">Faculty:</label>
+                </Form>
+            </div>
+        </div><br />
 
-             <!--this code below is for the dropdown menu-->
-             <select name="chair" id="chair">
-                <option value="Faculty of IT and Computing">Faculty of IT and Computing</option>
-                <option value="Faculty of Applied Sciences">Faculty of Applied Sciences</option>
-                <option value="Faculty of Agriculture">Faculty of Agriculture</option>
-                <option value="Faculty of Engineering">Faculty of Engineering</option> 
-            </select> <br /> <br />
-            <!--it ends here--> 
+        <div class="row">
+            <div class="col">
+                <!--this code is for the staff table-->
+                <Form method="POST" action="php_scripts/staff.php" id="staff" onsubmit="return staff ()">
+                    <div class="h3 text-center"><u>Staff Form</u></div>
+                    <label for="">Name:</label>
+                    <input type="text" id="dart" name="dart"> <br /> <br />
+                    <label for="">Department:</label>
 
-            <label for="">Laptop Model:</label>
-            <input type="text" id = "old" name="old" placeholder="Manufacturer's Name" > <br /> <br />
-            <label>Serial Number:</label>
-            <input type="text" id = "gender" name="gender"> <br /> <br />
+                    <!--this code below is for the dropdown menu-->
+                    <select name="shade" id="shade">
+                        <option value="Estates">Estates</option>
+                        <option value="Cleaning">Cleaning</option>
+                        <option value="Hotel and Accomodation">Hotel and Accomodation</option>
+                        <option value="Transport">Transport</option>
+                    </select> <br /> <br />
+                    <!--it ends here-->
 
-            <input type="submit" value="submit" id = "ClickMe" name="ClickMe_two"> 
-        
-    </Form>
+                    <label>Laptop Model:</label>
+                    <input type="text" id="bet" name="bet" placeholder="Manufacturer's Name"> <br /> <br />
+                    <label>Serial Number:</label>
+                    <input type="text" id="polite" name="polite"> <br /> <br />
+                    <button class="btn btn-primary">Submit</button>
 
-
-<!--this code is for the staff table-->
-    <Form method="POST" action="php_scripts/staff.php" id="staff"onsubmit="return staff ()">
-                <h2>Staff Registration Form</h2>
-            <label for="">Name:</label>
-            <input type="text" id = "dart" name="dart" > <br /> <br />
-            <label for="">Department:</label>
-
-            <!--this code below is for the dropdown menu-->
-            <select name="shade" id="shade">
-                <option value="Estates">Estates</option>
-                <option value="Cleaning">Cleaning</option>
-                <option value="Hotel and Accomodation">Hotel and Accomodation</option>
-                <option value="Transport">Transport</option> 
-            </select> <br /> <br />
-            <!--it ends here--> 
-
-            <label >Laptop Model:</label>
-            <input type="text" id = "bet" name="bet" placeholder="Manufacturer's Name" > <br /> <br />
-            <label>Serial Number:</label>
-            <input type="text" id = "polite" name="polite"> <br /> <br />
-        
-            <input type="submit" value="submit" id = "ClickMe" name="ClickMe_three">
-        
-    </Form>
+                </Form>
+            </div>
 
 
-<!--this code is for the visitor table-->
-    <Form method ="POST" action="php_scripts/visitor.php" id="visitor" onsubmit="return visitor()">
-                <h2>Visitor Registration Form</h2>
-            <label for="">Name:</label>
-            <input type="text" id = "clueless" name="clueless" > <br /> <br />
-            <label for="">Destination:</label>
+            <!--this code is for the visitor table-->
+            <div class="col">
+                <Form method="POST" action="php_scripts/visitor.php" id="visitor" onsubmit="return visitor()">
+                    <div class="h3 text-center"><u>Visitor Form</u></div>
+                    <label for="">Name:</label>
+                    <input type="text" id="clueless" name="clueless"> <br /> <br />
+                    <label for="">Destination:</label>
 
-             <!--this code below is for the dropdown menu-->
-             <select name="looking" id="looking">
-                <option value="Administration Block">Administration Block</option>
-                <option value="Hotel and Conference">Hotel and Conference</option>
-                <option value="Hostels">Hostels</option>
-                <option value="Stadia">Stadia</option> 
-            </select> <br /> <br />
-            <!--the value content is what is used in the php scripts.-->
-            <!--it ends here--> 
+                    <!--this code below is for the dropdown menu-->
+                    <select name="looking" id="looking">
+                        <option value="Administration Block">Administration Block</option>
+                        <option value="Hotel and Conference">Hotel and Conference</option>
+                        <option value="Hostels">Hostels</option>
+                        <option value="Stadia">Stadia</option>
+                    </select> <br /> <br />
+                    <!--the value content is what is used in the php scripts.-->
+                    <!--it ends here-->
 
-            <label for="">Laptop Model:</label>
-            <input type="text" id = "nervous" name="nervous" placeholder="Manufacturer's Name" > <br /> <br />
-            <label>Serial Number:</label>
-            <input type="text" id = "thanks" name="thanks"> <br /> <br />
-            
-            <input type="submit" value="submit" id = "ClickMe" name="ClickMe_four">
-    </Form>
-    
+                    <label for="">Laptop Model:</label>
+                    <input type="text" id="nervous" name="nervous" placeholder="Manufacturer's Name"> <br /> <br />
+                    <label>Serial Number:</label>
+                    <input type="text" id="thanks" name="thanks"> <br /> <br />
+                    <button class="btn btn-primary">Submit</button>
+                </Form>
+            </div>
+        </div>
+    </div>
+    </div>
 
-   <h3 id= "footer">Copyright &copy; 2018. All rigths reserved</h3> 
+    <!--this is the footer-->
+    <div class="footer h3 text-center">
+        Copyright &copy; 2018. All rights reserved
+    </div>
 
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -7,61 +7,211 @@
     <title>Homepage</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script type="text/javascript" src="main.js"></script>
+    <script src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
+    <!--this is the navigation bar -->
+    <div class="container">
+        <nav class="navbar navbar-dark fixed-top navbar-expand-md bg-dark">
+            <button type="button" class="nav-toggler" data-toggle="collapse" data-target="#myNav"><span class="navbar-toggler-icon"></span></button>
+            <div id="myNav" class="collapse navbar-collapse"><div class="h3 text-primary ml-5">Universal Security Systems Corporation</div>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a href="index.php" class="nav-link">HomePage</a></li>
+                    <li class="nav-item"><a href="Registration.php" class="nav-link">RegistrationPage</a></li>
+                    <li class="nav-item"><a href="https://www.google.com" class="nav-link">Google</a></li>
+                    <li class="nav-item"><a href="https://www.youtube.com" class="nav-link">YouTube</a></li>
+                    <li class="nav-item"><a href="https://www.youtube.com" class="nav-link">Contacts</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 
-    <hr />
-    <h1>Tenzy University College<em>(TUC)</em></h1>
-    <hr />
+    <!--this is the carousel-->
+    <div class="container">
+        <div id="john" class="carousel slide" data-ride="carousel">
+            <!--indicators-->
+            <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
 
-    <nav>
-        <ul>
-            <li> <a href="index.php" title="This is the HomePage"> Homepage</a> </li>
-            <li> <a href="Registration.php" title="This is the Sign in Page"> RegisterPage</a> </li>
-            <li> <a href="http://www.Google.com" title="Google.com"> Google.com</a> </li>
-            <li> <a href="http://www.YouTube.com" title="YouTube.com"> YouTube.com</a> </li>
-        </ul>
-    </nav>
+            <!--slideshow-->
+            <div class="carousel-inner fluid">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/current.jpeg" alt="First slide">
+                    <div class="carousel-caption"><span class="h4 text-primary">Security Starts with You!</span></div>
+                </div>
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="img/Beautiful.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/banner-img.jpg" alt="Third slide">
+                </div>
+            </div>
 
-    <fieldset>
-            <caption><b>ENTER RESPECTIVE DETAILS HERE</b></caption><br />
-            <hr color="black" cover="100%" />
+            <!--left and right controls-->
+            <div>
+                <a class="carousel-control-prev" href="#demo" data-slide="prev"><span class="carousel-control-prev-icon"></span></a>
+                <a class="carousel-control-next" href="#demo" data-slide="next"><span class="carousel-control-next-icon"></span></a>
+            </div>
+        </div>
+    </div><br />
+    <!--carousel ends here-->
 
-        <!--this code is for students display data-->
-        <form method="POST" action="display_php/student_display.php" onsubmit="return substitute1 ()">
-            <label for="">Student's Name:</label>
-            <input type="text" name="studentdisplay" id="mytextbox1" placeholder="Enter Name"> <br /> <br />
-            <input type="submit" value="submit" id="ClickMe" name="ClickMe"><br/>
-        </form>
 
-        <!--this code is for displaying lecturers data-->
-        <form method="POST" action="display_php/lecturer_display.php" onsubmit="return substitute2 ()">
-            <label for="">Lecturer's Name:</label>
-            <input type="text" name="lecturerdisplay" id="mytextbox2" placeholder="Enter Name"> <br /> <br />
-            <input type="submit" value="submit" id="ClickMe" name="ClickMe"><br/>
-        </form>
+    <!--forms-->
+    <div class="row">
+        <div class="col jumbotron">
+            <h4 class="text-center"><u>ENTER RESPECTIVE DETAILS HERE</u></h4><br />
+            <!--this code is for students display data-->
+            <form method="POST" action="display_php/student_display.php" onsubmit="return substitute1 ()">
+                <label for="">Student's Name:</label>
+                <input type="text" name="studentdisplay" id="mytextbox1" placeholder="Enter Name"> <br />
+                <button class="btn btn-primary">Submit</button>
+            </form>
 
-        <!--This code is for displaying staff data-->
-        <form method="POST" action="display_php/staff_display.php" onsubmit="return substitute3 ()">
-            <label for="">Staff Name:</label>
-            <input type="text" name="staffdisplay" id="mytextbox3" placeholder="Enter Name"> <br /> <br />
-            <input type="submit" value="submit" id="ClickMe" name="ClickMe"><br/>
-        </form>
+            <!--this code is for displaying lecturers data-->
+            <form method="POST" action="display_php/lecturer_display.php" onsubmit="return substitute2 ()">
+                <label for="">Lecturer's Name:</label>
+                <input type="text" name="lecturerdisplay" id="mytextbox2" placeholder="Enter Name"> <br />
+                <button class="btn btn-primary">Submit</button>
+            </form>
 
-        <!--This code is for displaying visitors data-->
-        <form method="POST" action="display_php/visitors_display.php" onsubmit="return substitute4 ()">
-            <label for="">Visitor's Name:</label>
-            <input type="text" name="visitorsdisplay" id="mytextbox4" placeholder="Enter Name"> <br /> <br />
-            <input type="submit" value="submit" id="ClickMe" name="ClickMe"><br/>
-        </form>
-    </fieldset>
-    <marquee>Security Starts with YOU!</marquee>
+            <!--This code is for displaying staff data-->
+            <form method="POST" action="display_php/staff_display.php" onsubmit="return substitute3 ()">
+                <label for="">Staff Name:</label>
+                <input type="text" name="staffdisplay" id="mytextbox3" placeholder="Enter Name"> <br />
+                <button class="btn btn-primary">Submit</button>
+            </form>
 
-    <h3>Copyright &copy; 2018. All rigths reserved</h3>
+            <!--This code is for displaying visitors data-->
+            <form method="POST" action="display_php/visitors_display.php" onsubmit="return substitute4 ()">
+                <label for="">Visitor's Name:</label>
+                <input type="text" name="visitorsdisplay" id="mytextbox4" placeholder="Enter Name"> <br />
+                <button class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+
+        <!--the memo section-->
+        <div class="col">
+            <h3 class="text-center"><u>STAFF MEMO</u></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga placeat dolorum nihil
+                aspernatur.Similique esse, veniam nobis iure voluptas ut nostrum? Commodi impedit perferendis dolores
+                deleniti
+                corporis accusamus sed id? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+                temporibus explicabo necessitatibus obcaecati
+                minus rerum placeat nam dolores? Doloremque tenetur repellat enim aliquam distinctio blanditiis fugit
+                vitae assumenda quos vel.</p>
+            <h3 class="text-center"><u>STUDENT MEMO</u></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga placeat dolorum nihil
+                aspernatur.Similique esse, veniam nobis iure voluptas ut nostrum? Commodi impedit perferendis dolores
+                deleniti
+                corporis accusamus sed id? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+                temporibus explicabo necessitatibus obcaecati
+                minus rerum placeat nam dolores? Doloremque tenetur repellat enim aliquam distinctio blanditiis fugit
+                vitae assumenda quos vel.</p>
+        </div>
+    </div>
+
+    <div class="container jumbotron">
+        <div class="h4 text-center">Other Services We Offer</div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                        <div class="card-header bg-success">Banking Systems</div>
+                        <div class="card-body">
+                            <img class="card-img-top img-fluid" src=img/buildings.jpeg> 
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis autem dolor
+                                aperiam eum,
+                                consequuntur vero aspernaturnecessitatibus exercitationem cupiditate hic voluptas culpa, iure
+                                incidunt optio
+                                quibusdam doloribus sapiente! Quisquam, quibusdam.</p>
+                        </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                        <div class="card-header bg-success">Web Development Services</div>
+                        <div class="card-body">
+                            <img class="card-img-top img-fluid" src=img/buildings.jpeg> 
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis autem dolor
+                                aperiam eum,
+                                consequuntur vero aspernaturnecessitatibus exercitationem cupiditate hic voluptas culpa, iure
+                                incidunt optio
+                                quibusdam doloribus sapiente! Quisquam, quibusdam.</p>
+                        </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="card">
+                        <div class="card-header bg-success">ICT Consoltancy</div>
+                        <div class="card-body">
+                            <img class="card-img-top img-fluid" src=img/buildings.jpeg> 
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis autem dolor
+                                aperiam eum,
+                                consequuntur vero aspernaturnecessitatibus exercitationem cupiditate hic voluptas culpa, iure
+                                incidunt optio
+                                quibusdam doloribus sapiente! Quisquam, quibusdam.</p>
+                        </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="card">
+                        <div class="card-header bg-success">Systems Repair and Upadation</div>
+                        <div class="card-body">
+                            <img class="card-img-top img-fluid" src=img/buildings.jpeg> 
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis autem dolor
+                                aperiam eum,
+                                consequuntur vero aspernaturnecessitatibus exercitationem cupiditate hic voluptas culpa, iure
+                                incidunt optio
+                                quibusdam doloribus sapiente! Quisquam, quibusdam.</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--this is the patners section-->
+    <div class="container">
+            <div class="h3 text-underlined"><u>Our Patners</u></div>
+            <div class="row">
+                <div class="col"><img src="img/rural.jpeg" alt="The image is displayed here" class="img-fluid"></div>
+                <div class="col"><img src="img/farm.jpeg" alt="Sorry no image for YOU!!!" class="img-fluid"></div>
+                <div class="col"><img src="img/country.jpeg" alt="Sorry bruh no image!!!" class="img-fluid"></div>
+                <div class="col"><img src="img/banner-img.jpeg" alt="NO picture for you" class="img-fluid"></div><br />
+            </div><br />
+        </div>
+
+        <!--this is the footer section-->
+    <div class="row bg-success ">
+        <div class="col">
+            <footer class="page-footer">
+                <div class="footer-copyright text-left py-3"><span class="h6 ml-5">Copyright &copy 2019. All rights
+                        reserved.</span></div>
+                <div class="col">
+                    <div class="text-center mb-2">
+                        <a href="https://www.twitter.com/johnlihemo" title="twitter handle"><i class="fa fa-twitter-square fa-lg "></i></a>
+                        <a href="https://www.facebook.com/johnlukaku" title="facebook page"><i class="fa fa-facebook-square fa-lg"></i></a>
+                        <a href="https://www.instagram.com/johnlihemo" title="instagram account"><i class="fa fa-instagram fa-lg"></i></a>
+                        <a href="https://www.linkedin.com/johnlihemo" title="linkedin account"><i class="fa fa-linkedin-square fa-lg"></i></a>
+                        <a href="https://www.github.com/johnlihemo" title="github repo"><i class="fa fa-github-square fa-lg"></i></a>
+                    </div>
+                </div>
+        </div>
+        </footer>
+    </div>
+
 
 </body>
+
 
 </html>
